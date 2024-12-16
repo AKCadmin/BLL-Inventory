ca34c5fbdce24f9b8df35818078cda0d7247f148
anandsen9237 <anand.sen@techwens.in>
2024-12-16 18:15:53 +0530

    bugs

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -9,6 +9,6 @@ class Product extends Model
    protected $table = "products";
     public function company()
     {
-        return $this->hasOne(company::class,'id','company_id');
+        return $this->hasOne(Company::class,'id','company_id');
     }
 }
