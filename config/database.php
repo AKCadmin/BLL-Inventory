5ed030692369281f8f4a5d554859e1dfb9149eda
anand481 <anand.sen@techwens.in>
2025-05-11 23:25:56 +0530

    changes

--- config/database.php
+++ config/database.php
@@ -102,7 +102,7 @@ return [
             'url' => env('DB_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
-            'database' => 'bll_inventory_new4',
+            'database' => 'bll_inventory',
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', ''),
             'charset' => env('DB_CHARSET', 'utf8'),
