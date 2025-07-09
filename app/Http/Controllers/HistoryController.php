86ce16b3e7d296bd0e70579a62465e7f84077426
anand481 <anand.sen@techwens.in>
2025-07-09 19:55:55 +0530

    changes

--- app/Http/Controllers/HistoryController.php
+++ app/Http/Controllers/HistoryController.php
@@ -159,10 +159,10 @@ public function purchaseHistoryShow($id, $encodedCreatedAt, $noOfcartoon)
             ->where('batches.no_of_units', $noOfcartoon)
             ->whereRaw('DATE(batches.created_at) = ?', [$createdAt])
             ->get();
-dd($batchData);
-        if ($batchData->isEmpty()) {
-            return redirect()->route('stock.list')->with('error', 'No stock batches found for the selected product.');
-        }
+// dd($batchData);
+        // if ($batchData->isEmpty()) {
+        //     return redirect()->route('stock.list')->with('error', 'No stock batches found for the selected product.');
+        // }
 
         $groupedData = $batchData->groupBy('product_id')->map(function ($items) use ($brand) {
             $product = $items->first();
